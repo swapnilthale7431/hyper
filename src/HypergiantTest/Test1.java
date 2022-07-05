3183c5892c43b37da4a096477c1ec37e5ed2706a
Swapnil <swapnilthale7431@gmail.com>
2022-07-05 13:25:54 +0530

    second commit

---
+++ src/HypergiantTest/Test1.java
@@ -0,0 +1,17 @@
+package HypergiantTest;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@BeforeMethod
+	public void OpenBrowser() {
+
+	}
+	@Test
+	public void m() {
+		
+	}
+	
+}
